8163927d1f40125b8d505ebf859c99789d46e640
Y@$HMuNdE : ) <109969817+MundeYash@users.noreply.github.com>
2026-01-05 17:05:55 +0000

    testing setup github actions pipeline via code-space

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,8 +3,14 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>hello world</div>
-      <button className="btn">click me</button>
+      <div className="parent">
+        <div>hello world</div>
+        <button className="btn">click me</button>
+        <p>
+          Now this React code is linked with githubAction pipeline and now can
+          you test the code as well
+        </p>
+      </div>
     </>
   );
 }
